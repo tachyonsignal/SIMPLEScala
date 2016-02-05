f1cfcdf0e044a421e73ce89bc7564b9503279960
louis <cornfutes@gmail.com>
2016-02-05 13:13:48 -0800

    Boolean primitive

--- src/Primitives.scala
+++ src/Primitives.scala
@@ -1,4 +1,19 @@
+// Represents a Integer type in Simple
 class SimpleNumber(_value : Number) extends Primitive {
+
+  val value = _value
+
+  def isReducible() : Boolean = false
+
+  override def toString(): String = { value.toString }
+
+  // irreducible reduces to itself
+  def reduce() : Expression = this
+
+}
+
+// Represents a boolean type in SIMPLE
+class SimpleBoolean(_value : Boolean) extends Primitive {
 
   val value = _value
 
